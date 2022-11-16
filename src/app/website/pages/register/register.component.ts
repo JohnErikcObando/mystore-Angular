dec18a8cf7ae01673b71d8a79e72b3889e35f1aa
JohnErikcObando <Jeobando11@hotmail.com>
2022-11-16 11:05:16 -0500

    my-store-angular

---
+++ src/app/website/pages/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { OnExit } from '../../../guards/exit.guard';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnExit {
+
+  constructor() { }
+
+
+  onExit() {
+    const rta = confirm('Logica desde componen, Estas seguro Salir?');
+    return rta;
+  }
+
+}
